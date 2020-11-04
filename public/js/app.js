6c83b4ea5b96ddbf520da2e0d0b958c9a453fcb2
Alessio Bonetti <alesine87@gmail.com>
2020-11-04 16:26:41 +0100

    svuotata table fumetti e rinominato migration comics

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\alesi\Desktop\Boolean\Esercizi\laravel-migration-seeder\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\alesi\Desktop\Boolean\Esercizi\laravel-migration-seeder\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\alesi\Desktop\Boolean\Esercizi\laravel-relations\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\alesi\Desktop\Boolean\Esercizi\laravel-relations\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
